55c68f1ca4544eca52d56168ddb2b9ed97203b03
vishalmanglaw453 <vishalmanglaw@gmail.com>
2023-12-14 12:28:51 +0530

    z index mistake

--- assets/js/js.js
+++ assets/js/js.js
@@ -6,7 +6,7 @@ function openNav() {
    
   let mybutton = document.getElementById("myBtn");
 
-  // When the user scrolls down 20px from the top of the document, show the button
+ 
   window.onscroll = function() {scrollFunction()};
   
   function scrollFunction() {
@@ -17,7 +17,6 @@ function openNav() {
     }
   }
   
-  // When the user clicks on the button, scroll to the top of the document
   function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
